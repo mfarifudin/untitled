14d0b01686dd80fa86956dab27e99caf95d7ec7d
mfarifudin <fatkhan@outlook.com>
2015-06-24 04:29:25 +0700

    v1.0

---
+++ coba.py
@@ -0,0 +1,41 @@
+__author__ = 'Muh Fatkhan Arifudin'
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import mysql.connector
+import time
+import json
+import sys
+
+conn = mysql.connector.connect(user='root', password='', host='127.0.0.1',database='tweets')
+
+c = conn.cursor()
+
+ckey = "hRNLzt3eIxSmSdyR7AZYo7tX6"
+csecret = "lfvK9Knc47vdYqK9STa0LzUwqwGPyaHukhRH9HG8dutTt2glEZ"
+atoken = "81080657-XO1oal47uPWikhjCK5EFaW9dt1FvsbyJJQ45GToX0"
+asecret = "kvjNLExUpGNN7rcXu5vpoehktf0Fu9MI9blSfEAqJKSvl"
+
+auth = tweepy.OAuthHandler(ckey,csecret)
+auth.set_access_token(atoken, asecret)
+
+class listener(StreamListener):
+    def on_data(self, data):
+        all_data = json.loads(data)
+        post = all_data["text"]
+        username = all_data["user"]["screen_name"]
+
+        c.execute("INSERT INTO tweets (time, username, post) VALUES (%s,%s,%s)",
+            (time.time(), username, post))
+
+        conn.commit()
+
+        print(username, post)
+
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["jokowi"])
---
+++ coba3.py
@@ -0,0 +1,97 @@
+__author__ = 'Muh Fatkhan Arifudin'
+import tweepy
+import sys
+import mysql.connector
+import jsonpickle
+import json
+import time
+
+
+conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='tweets')
+
+c = conn.cursor()
+
+ckey = "hRNLzt3eIxSmSdyR7AZYo7tX6"
+csecret = "lfvK9Knc47vdYqK9STa0LzUwqwGPyaHukhRH9HG8dutTt2glEZ"
+atoken = "81080657-XO1oal47uPWikhjCK5EFaW9dt1FvsbyJJQ45GToX0"
+asecret = "kvjNLExUpGNN7rcXu5vpoehktf0Fu9MI9blSfEAqJKSvl"
+
+auth = tweepy.AppAuthHandler(ckey, csecret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
+
+if (not api):
+    print("Can't Authenticate")
+    sys.exit(-1)
+
+searchQuery = 'jokowi'  # this is what we're searching for
+maxTweets = 2000000  # Some arbitrary large number
+tweetsPerQry = 100
+
+# If results from a specific ID onwards are reqd, set since_id to that ID.
+# else default to no lower limit, go as far back as API allows
+sinceId = None
+
+# If results only below a specific ID are, set max_id to that ID.
+# else default to no upper limit, start from the most recent tweet matching the search query.
+max_id = 612527988249264128
+
+def print_tweet(tweet):
+    username = tweet.user.screen_name
+    post = tweet.text
+    id = tweet.id
+
+    c.execute("INSERT INTO jokowi (id, time, username, post) VALUES (%s,%s,%s,%s)",
+                 (id, time.time(), username, post))
+    conn.commit()
+
+tweetCount = 0
+print("Downloading max {0} tweets".format(maxTweets))
+while tweetCount < maxTweets:
+    try:
+        if (max_id <= 0):
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
+                for i in range (0, 99):
+                    tweets = new_tweets[i]
+                    print_tweet(tweets)
+            else:
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        since_id=sinceId)
+                for i in range (0, 99):
+                    tweets = new_tweets[i]
+                    print_tweet(tweets)
+        else:
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        max_id=str(max_id - 1))
+                for i in range (0, 99):
+                    tweets = new_tweets[i]
+                    print_tweet(tweets)
+            else:
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        max_id=str(max_id - 1),
+                                        since_id=sinceId)
+                for i in range (0, 99):
+                    tweets = new_tweets[i]
+                    print_tweet(tweets)
+        if not new_tweets:
+            print("No more tweets found")
+            break
+        '''
+        tweet=new_tweets[0]
+        for param in dir(tweet):
+            if not param.startswith("_"):
+                print ("%s : %s" % (param, eval("tweet." + param)))
+'''
+        tweetCount += len(new_tweets)
+        print("Downloaded {0} tweets".format(tweetCount))
+        max_id = new_tweets[-1].id
+
+    except tweepy.TweepError as e:
+        # Just exit if any error
+        print("some error : " + str(e))
+        break
+
+        #print("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
